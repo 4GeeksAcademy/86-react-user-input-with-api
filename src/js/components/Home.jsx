f63931affbbe58b1f70894b5ff96d2d7d4c8dd27
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-10-31 23:24:50 +0000

    to be cont

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,58 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, createRef } from "react";
 
 //create your first component
 const Home = () => {
 	const [groceries, setGroceries] = useState([
-		{ value: 'Apple', obtained: false, edit: false },
-		{ value: 'Bananas', obtained: false, edit: false },
-		{ value: 'Cantelop', obtained: false, edit: false }])
+		{ label: 'Apple', is_done: false },
+		{ label: 'Bananas', is_done: false },
+		{ label: 'Cantelop', is_done: false }])
 	const [userInput, setUserInput] = useState("");
-	// const [editMode, setEditMode] = useState(false);
 	const [updatedValue, setUpdatedValue] = useState("");
+	// const [currentUser, setCurrentUser] = useState();
 
-	//{value: grocery name, obtained: true}
-	//this is the js of a react component
+	useEffect(() => {
+		getUser()
+	}, [])
+
+	useEffect(() => {
+		console.log(groceries)
+	}, [groceries])
+
+	async function getUser() {
+		let getResponse = await fetch("https://playground.4geeks.com/todo/users/valerieclaire96")
+		let getData = await getResponse.json()
+		if (getData?.detail?.includes("doesn't exist")) {
+			createUser
+		}
+		else {
+			setGroceries(getData.todos)
+		}
+	}
+	async function createUser() {
+		let postResponse = await fetch("https://playground.4geeks.com/todo/users/valerieclaire96", {
+			method: "POST",
+			headers: { "Content-type": "application/json" },
+		})
+		let postData = await postResponse.json()
+		console.log(postData)
+	}
+
 	const addToList = (item, event) => {
+		//we will want to add a fetch request
 		if (event.key === "Enter" || event.type == "click") {
-			let groceryItem = {value: item, obtained: false, edit: false}
+			let groceryItem = { label: item, is_done: false }
 			//without the spread operator [[Apple, banana, cantelop], dragonfruit]
 			//with the spread operator [Apple, banana, cantelop, dragonfruit]
+
 			setGroceries([...groceries, groceryItem])
 			setUserInput("")
 		}
 	}
 	const updateItem = (index) => {
 		let newList = []
-		console.log(groceries[index]['edit'])
-		if (groceries[index]['edit']) {
-			newList = groceries.map((grocery, i) => {
-				if (index == i) {
-					return { value: updatedValue, obtained: grocery['obtained'], edit: !grocery['edit'] };
-				}
-				else {
-					return grocery;
-				}
-			})
-			console.log(newList)
-		}
-		else {
-			newList = groceries.map((grocery, i) => {
-				if (index == i) {
-					return { value: grocery['value'], obtained: grocery['obtained'], edit: !grocery['edit'] };
-				}
-				else {
-					return grocery;
-				}
-			})
-		}
 
-		setGroceries(newList);
-		setUpdatedValue("")
 	}
 	const markItemAsShopped = (index) => {
 		let newList = groceries.map((grocery, i) => {
 			if (index == i) {
-				return { value: grocery['value'], obtained: !grocery['obtained'] };
+				return { label: grocery['label'], is_done: !grocery['obtained'] };
 			}
 			else {
 				return grocery;
@@ -66,7 +69,7 @@ const Home = () => {
 			<h3>Need From The Store.</h3>
 			<input
 				type="text"
-				placeholder="Enter text here"
+				placeholder="Enter grocery item here"
 				value={userInput}
 				onChange={(e) => setUserInput(e.target.value)}
 				onKeyDown={(e) => addToList(userInput, e)}
@@ -76,17 +79,7 @@ const Home = () => {
 				{/* if we want to write js/dynamic code we must use {} */}
 				{groceries.map((grocery, index) => {
 					return (
-						<li className={grocery.obtained ? "shopped" : ""} key={index}>{grocery.value}
-							{grocery.edit ?
-								<span>
-									<input type="text"
-										onChange={(e) => setUpdatedValue(e.target.value)}
-									/>
-
-									<span onClick={() => updateItem(index)}>💾</span>
-								</span>
-								: <span onClick={() => updateItem(index)}>✏️</span>
-							}
+						<li className={grocery.is_done ? "shopped" : ""} key={index}>{grocery.label}
 							<span onClick={() => markItemAsShopped(index)}>☑️</span>
 						</li>
 					)
